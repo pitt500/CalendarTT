631d35fce5814d7902493aacc26e370bf718ad34
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-23 00:50:56 -0700

    Create CalendarTT xcode project

---
+++ CalendarTT/CalendarTT-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalendarTT-Demo
+//
+//  Created by projas on 12/23/19.
+//  Copyright © 2019 Pedro Rojas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CalendarTT/CalendarTT/CalendarTT.h
@@ -0,0 +1,19 @@
+//
+//  CalendarTT.h
+//  CalendarTT
+//
+//  Created by projas on 12/23/19.
+//  Copyright © 2019 Pedro Rojas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarTT.
+FOUNDATION_EXPORT double CalendarTTVersionNumber;
+
+//! Project version string for CalendarTT.
+FOUNDATION_EXPORT const unsigned char CalendarTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarTT/PublicHeader.h>
+
+
